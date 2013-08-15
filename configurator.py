96f8f38afe6245a4e9308310537bcd333c267558
egghead <thomas.omans@gmail.com>
2013-08-15 07:11:41 +0000

    pep8 & pprint

--- configurator.py
+++ configurator.py
@@ -2,8 +2,10 @@ from os import path, walk, getcwd
 from copy import deepcopy
 from yaml import load
 
+
 YAML_FORMATS = ["yml", "yaml"]
 BASE_CONFIG = "base"
+
 
 def find_files_by_names(directory, names):
     """ walks a directory structure
@@ -14,6 +16,7 @@ def find_files_by_names(directory, names):
         files += filter(lambda f: path.isfile(f), filenames)
     return files
 
+
 def filenames_by_exts(exts, *filenames):
     """ given ['json', 'ini'], configurator returns
         ['configurator.json', 'configurator.ini'] """
@@ -22,14 +25,17 @@ def filenames_by_exts(exts, *filenames):
         fnames += map(lambda ext: "{0}.{1}".format(filename, ext), exts)
     return fnames
 
+
 def find_config_paths(basedir, *environments):
     """ search for filenames matching the config format """
     filenames = filenames_by_exts(YAML_FORMATS, BASE_CONFIG, *environments)
     return find_files_by_names(basedir, filenames)
 
+
 def filepath_to_namespace(path, basedir):
     """ given ./foo/bar/baz.yml returns ['foo', 'bar'] """
     return path[len(basedir):].lstrip("/").split("/")[:-1]
+
 
 def namespace_node(namespace, node):
     """ given ['a', 'b', 'c'], 23 returns {a: {b: {c: 23}}} """
@@ -44,6 +50,7 @@ def namespace_node(namespace, node):
         ref = ref[name]
     return obj
 
+
 def load_namespaced_yaml(path, basedir):
     """ Loads a yaml document namespacing it by the directory structure
         relative to basedir """
@@ -54,12 +61,15 @@ def load_namespaced_yaml(path, basedir):
     else:
         return config
 
+
 def paths_to_configs(paths, basedir):
     return map(lambda path: load_namespaced_yaml(path, basedir), paths)
 
+
 def deep_merge(a, b):
-    """ recursively merges dict's
-        borrowed from: http://www.xormedia.com/recursively-merge-dictionaries-in-python/ """
+    """ recursively merges dicts """
+    # borrowed from
+    # http://www.xormedia.com/recursively-merge-dictionaries-in-python/
     if not isinstance(b, dict):
         return b
     result = deepcopy(a)
@@ -70,6 +80,7 @@ def deep_merge(a, b):
             result[k] = deepcopy(v)
     return result
 
+
 def merge_configs(configs):
     """ more like partial reduce deep_merge amirite """
     result = {}
@@ -77,9 +88,12 @@ def merge_configs(configs):
         result = deep_merge(result, config)
     return result
 
+
 if __name__ == "__main__":
-    print("Loading up hyper kernels...")
     basedir = getcwd()
     paths = find_config_paths(basedir)
     configs = paths_to_configs(paths, basedir)
-    print(merge_configs(configs))
+    config = merge_configs(configs)
+
+    from pprint import pprint
+    pprint(config)
