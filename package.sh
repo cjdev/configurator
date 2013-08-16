bc4bb9c7ba910643f6793ce51b183d174aabc637
Darrell Hamilton <darrell.noice@gmail.com>
2013-08-16 16:18:03 -0700

    parameterize python interpreter

--- package.sh
+++ package.sh
@@ -5,9 +5,14 @@ set -e
 PYINSTALLER_URL='http://downloads.sourceforge.net/project/pyinstaller/2.0/pyinstaller-2.0.tar.bz2'
 PYINSTALLER_EXE='dist-utils/pyinstaller-2.0/pyinstaller.py'
 
+if [ -z "${PYTHON_EXE}" ]
+then
+  PYTHON_EXE=python
+fi
+
 if [ ! -f $PYINSTALLER_EXE ]; then
     mkdir -p dist-utils
     curl -Ls $PYINSTALLER_URL | tar -C 'dist-utils' -xj 
 fi
 
-python $PYINSTALLER_EXE --onefile configurator.py -o .
+$PYTHON_EXE $PYINSTALLER_EXE --onefile configurator.py -o .
