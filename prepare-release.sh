7fe3b3f59f2da26473984ce021bc908b3bf79e93
Darrell Hamilton <darrell.noice@gmail.com>
2013-08-15 17:23:07 -0700

    include LICENSE in the release tarball

--- prepare-release.sh
+++ prepare-release.sh
@@ -2,13 +2,19 @@
 
 set -e
 
-PKG_DIR="configurator-$(uname -srm | tr ' ' '-')"
+if [ -n $1 ]
+then
+  VERSION="-${1}"
+fi
+
+PKG_DIR="configurator${VERSION}-$(uname -srm | tr ' ' '-')"
 DIST_DIR=./dist
 DIST=${DIST_DIR}/configurator
 
 mkdir -p ${DIST_DIR}/${PKG_DIR}
 
 cp ${DIST} ${DIST_DIR}/${PKG_DIR}
+cp LICENSE ${DIST_DIR}/${PKG_DIR}
 
 tar -czf ${DIST_DIR}/${PKG_DIR}.tar.gz -C ${DIST_DIR} ${PKG_DIR}
 
